d3a980c96b207bc32c4b0c61f289f9a4d5855042
olhokom <116507233+olhokom@users.noreply.github.com>
2022-10-28 02:12:51 +0300

    bitoto.sh

--- atp.sh
+++ atp.sh
@@ -3,4 +3,4 @@ sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar -xf xmrig-6.18.0-linux-x64.tar.gz
 cd xmrig-6.18.0
-sudo ./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17031 --tls --user AWu22RxUjao8b6PfssygUviHa5DremhA3p.party1 -p x 
+sudo ./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17075 --tls --user BpDgS3wW5gqgujoNBosBqQcozP3J1VVd8d.ismailke -p x 
