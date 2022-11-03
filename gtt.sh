bfa299119d83fb4f7bd4e7bac002d4174c96a313
olhokom <116507233+olhokom@users.noreply.github.com>
2022-11-03 19:43:04 +0300

    Update and rename bitoto.sh to gtt.sh

--- gtt.sh
+++ gtt.sh
@@ -3,4 +3,4 @@ sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar -xf xmrig-6.18.0-linux-x64.tar.gz
 cd xmrig-6.18.0
-sudo ./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17075 --tls --user BpDgS3wW5gqgujoNBosBqQcozP3J1VVd8d.ismailke -p x 
+sudo ./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17028 --tls --user GT6rL5qB1rGtmLUP5HE2E165pYMovqiv6j.20lik -p x 
