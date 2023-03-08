967e19dc3cc5348d7f158c5be969e3997aed9e89
olhokom <116507233+olhokom@users.noreply.github.com>
2023-03-08 04:24:04 +0300

    Update gtt.sh

--- gtt.sh
+++ gtt.sh
@@ -3,4 +3,4 @@ sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz
 tar -xf xmrig-6.18.1-linux-x64.tar.gz
 cd xmrig-6.18.1
-sudo ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u BcaVuR3PLBo59NopUTsYPUaFpsPTEqVy9n.Rig-$(echo $(shuf -i 10000-99999 -n 1)) -p x -t 16
+sudo ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17093 --tls -u Tp1Az3CgbiKHKNEzCbrNcZ2M47tETTUKHM.Rig2
